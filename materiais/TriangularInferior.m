6aab70550809225bea6882a31e30c3dfb459ccec
ensino-fct <129012121+ensino-fct@users.noreply.github.com>
2024-08-21 19:49:32 -0300

    Add files via upload

---
+++ materiais/TriangularInferior.m
@@ -0,0 +1,12 @@
+function x=TriangularInferior(A,b)
+  n=length(A);
+  x(1) = b(1)/A(1,1);
+  for i=2:n
+    soma=0;
+    for k=1:i-1
+      soma=soma+A(i,k)*x(k);
+    endfor
+    x(i) = (b(i) - soma)/A(i,i);
+  endfor
+
+  endfunction
